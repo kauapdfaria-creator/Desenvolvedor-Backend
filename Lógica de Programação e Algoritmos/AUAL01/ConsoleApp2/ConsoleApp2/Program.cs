0feb77f8cbd10cb4e372e42aca5ed8b5848e1921
kaua <kauapdfaria@gmail.com>
2026-07-17 20:46:14 -0300

    aula 1 cesar

---
+++ Lógica de Programação e Algoritmos/AUAL01/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http.Headers;
+
+double x, y, media;
+Console.WriteLine("digite o propio numero:");
+x = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundop nimero: ");
+y = double.Parse(Console.ReadLine());
+media = (x + y) / 2.0;
+Console.WriteLine("Media=" + media);
