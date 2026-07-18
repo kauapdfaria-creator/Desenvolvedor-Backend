b6ac0c005c14a4ad9d70be27e07aec93fbdd59ee
kaua <kauapdfaria@gmail.com>
2026-07-17 21:53:07 -0300

    finalizado aula dia 17/07/26

---
+++ Lógica de Programação e Algoritmos/AUAL01/ConsoleApp2/saidadedados/saidadedados/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+int n;
+int numero = 12;
+double flutuante = 2.85;
+string paçavra = "SENAI";
+char caracter = 'S';
+bool logica1 = true;
+bool logica2 = false;
+    
+
+WriteLine("SENAI Americana");
+WriteLine("Curso DEV BackEND");
+WriteLine("Argentina");
+Write("Argentina");
+ReadKey();
