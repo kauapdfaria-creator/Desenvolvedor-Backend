f87d465bd35417bdc51140ee680899b66cc214c2
kaua <kauapdfaria@gmail.com>
2026-07-20 21:56:08 -0300

    Declaraçao de variaveis ,entrada, Processamento e saisa de dados

---
+++ Lógica de Programação e Algoritmos/AUAL01/ConsoleApp2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Declaraçao de variaveis 
+double largura;
+double comprimento;
+double area;
+double valor;
+double preco;
+
+//Entrda de dados 
+Console.Write("Digite a largura do terreno");
+largura = double.Parse(Console.ReadLine());
+Console.Write("Digite o comprimento do terreno: ");
+comprimento = double.Parse(Console.ReadLine());
+Console.Write("digite o valor do metro quadrado do terreno");
+valor = double.Parse(Console.ReadLine());
+
+//Processamento de dados
+area = largura * comprimento;
+preco = area * valor;
+
+//saida de dados 
+Console.WriteLine($"À`rea do terreno: {area:n}\nPreço do terreno: R$ {preco:n}");
---
+++ Lógica de Programação e Algoritmos/AUAL01/ConsoleApp2/exercicioretangulo/exercicioretangulo/Program.cs
@@ -0,0 +1,25 @@
+﻿//Criaçao das variaveis 
+double bAse;
+double altura;
+double area;
+double perimetro;
+double diagonal;
+
+//Entrada de dados
+Console.Write("Digite a base do retangulo : ");
+bAse = double.Parse(Console.ReadLine());
+ Console.Write("Digite a altura do retangulo : ");
+altura = double.(Parse(consoLe.ReadLine;());
+
+
+//Processamento de dados 
+area = bAse + altura + bAse + altura;
+perimetro = bAse + altura + bAse + altura;
+diagonal = Math.Sqrt(Math.Pow(bAse, 2) + Math.Pow(altura, 2));
+
+//saida de dados 
+Console.WriteLine($"--Dados do retangulo--\n" +
+    $"area: {area\n}"
+    $"perimetro: {perimetro:f4}\n" +
+    $"Diagonal: {diagonal:n}\n");
+
